3c7492615039882880eb20d7a95cd6396997ed75
overwatcheddude <overwatcheddude@gmail.com>
2019-04-28 12:18:14 +0400

    Added graphview library

--- app/src/main/java/com/example/user/spacecraftoperationsapplication/Rover.java
+++ app/src/main/java/com/example/user/spacecraftoperationsapplication/Rover.java
@@ -15,6 +15,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.w3c.dom.Text;
@@ -235,5 +239,16 @@ public class Rover extends AppCompatActivity {
         checkSubscriptions(R.id.power_telemetry_motor_rr_I);
 
         setTime(R.id.tvTime);
+
+        //Graph
+        GraphView graph = findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
     }
 }
